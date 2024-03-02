66d7979c54f517dbb107976fe1ca3978cc3e7321
Mateusz Kowalczyk <mateusz.kowalczyk.0305@gmail.com>
2024-03-02 13:35:33 +0100

    Próba użycia potencjometru.

---
+++ potencjometr/potencjometr.ino
@@ -0,0 +1,14 @@
+int value;
+
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+pinMode(A0, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+value = analogRead(A0);
+Serial.println(value);
+delay(500);
+}
